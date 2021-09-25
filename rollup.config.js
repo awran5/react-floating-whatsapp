4f28627c17bb24acaf48e4dbce963337d62840d5
awran5 <awran5@yahoo.com>
2021-09-25 04:52:58 +0200

    rollup typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import autoprefixer from 'autoprefixer'
 import copy from 'rollup-plugin-copy'
-
 import { terser } from 'rollup-plugin-terser'
 import packageJson from './package.json'
 
